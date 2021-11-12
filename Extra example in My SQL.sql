76e4c8a0c68822d767e7da3fd0372132e4ac66d0
Tanisha Patel <taishakabariya15@gamil.com>
2021-11-12 20:17:37 +0530

    My SQL extra example of select query.

---
+++ Extra example in My SQL.sql
@@ -0,0 +1,29 @@
+-- Select query to get name (firstname + lastname), department name, country name, job name
+
+SELECT 
+concat (emp.firstname, ' ' , emp.lastname) name,
+d.name department_name, 
+c.name country_name,
+j.name job_name 
+FROM employee emp
+JOIN department d ON emp.fk_department_id = d.id
+JOIN country c ON d.fk_country_id = c.id
+JOIN job j ON emp.fk_job_id = j.id
+
+
+-- Select query to get  2nd highest salary from all the employees
+
+
+SELECT DISTINCT salary 
+FROM employee emp1 
+WHERE 2=
+(SELECT count(DISTINCT salary) 
+FROM employee emp2
+WHERE emp1.salary<=emp2.salary);
+
+
+-- Select query to get  all job name and department name in single query
+
+SELECT name FROM job
+UNION
+SELECT name FROM department
